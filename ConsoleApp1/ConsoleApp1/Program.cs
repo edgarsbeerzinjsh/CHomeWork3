c296a6a6e105a3c816cbfc826df053b79533640a
EdgarsB <edgars.beerzinjsh@gmail.com>
2022-04-22 23:21:52 +0300

    1. Task

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+// See https://aka.ms/new-console-template for more information
+// Simbolu daudzbveidībai programmā
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("1. Uzdevums: Telefons");
+Telephone telephone1 = new Telephone();
+
+Console.WriteLine($"Ievadījāt telefonu ar šādiem parametriem:");
+Console.WriteLine($"Marka: {telephone1.Mark}");
+Console.WriteLine($"Modelis: {telephone1.Model}");
+Console.WriteLine($"Izmēri: {telephone1.Hight} X {telephone1.Width} X {telephone1.Depth}");
+Console.WriteLine();
+telephone1.Call();
+telephone1.Sms();
+
+
+Console.WriteLine();
+Console.WriteLine();
+Telephone telephone2 = new Telephone();
+
+Console.WriteLine($"Ievadījāt telefonu ar šādiem parametriem:");
+Console.WriteLine($"Marka: {telephone2.Mark}");
+Console.WriteLine($"Modelis: {telephone2.Model}");
+Console.WriteLine($"Izmēri: {telephone2.Hight} X {telephone2.Width} X {telephone2.Depth}");
+Console.WriteLine();
+telephone2.Call();
+telephone2.Sms();
--- ConsoleApp1/ConsoleApp1/Telephone.cs
+++ ConsoleApp1/ConsoleApp1/Telephone.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Telephone
+    public class Telephone
     {
+        public Telephone()
+        {
+            Console.WriteLine($"Ievadiet telefona marku");
+            Mark = Console.ReadLine();
+            Console.WriteLine($"Ievadiet telefona modeli");
+            Model = Console.ReadLine();
+            Console.WriteLine($"Ievadiet telefona garuma izmēru");
+            Hight = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Ievadiet telefona platuma izmēru");
+            Width = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Ievadiet telefona augstuma izmēru");
+            Depth = double.Parse(Console.ReadLine());
+        }
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public double Hight { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public void Call()
+        {
+            Console.WriteLine($"Ar {Mark} {Model} ir iespējams veikt zvanus");
+        }
+        public void Sms()
+        {
+            Console.WriteLine($"Ar {Mark} {Model} ir iespējams sūtīt SMS");
+        }
+        
     }
 }

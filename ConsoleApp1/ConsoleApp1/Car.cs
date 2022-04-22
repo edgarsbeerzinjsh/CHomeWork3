6da65a73ae0054c3a934c0ecb8455b1b6765f07c
EdgarsB <edgars.beerzinjsh@gmail.com>
2022-04-23 00:02:26 +0300

    +2.Task

---
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp1
+{
+    public class Car
+    {
+        public Car()
+        {
+            Console.WriteLine($"Ievadiet auto marku");
+            Mark = Console.ReadLine();
+            Console.WriteLine($"Ievadiet auto numurzīmi");
+            Number = Console.ReadLine();
+            Console.WriteLine($"Ievadiet auto ātrumu");
+            Speed = double.Parse(Console.ReadLine());
+        }
+        public string Mark { get; set; }
+        public string Number { get; set; }
+        public double Speed { get; set; }
+
+        public void StartDrive()
+        {
+            Speed = 10;
+            Console.WriteLine($"{Mark} ar numurzīmi {Number} uzsāk braukt ar ātrumu {Speed} km/h");
+        }
+        public void AddSpeed()
+        {
+            Console.WriteLine($"Ievadiet par cik km/h jāpalielina auto ātrums:");
+            double addSpeed = double.Parse(Console.ReadLine());
+            Speed += addSpeed; 
+            Console.WriteLine($"{Mark} ar numurzīmi {Number} pārvietojas ar {Speed} km/h");
+        }
+        public void ReduceSpeed()
+        {
+            Console.WriteLine($"Ievadiet par cik km/h jāsamazina auto ātrums:");
+            double reduceSpeed = double.Parse(Console.ReadLine());
+            Speed -= reduceSpeed;
+            Console.WriteLine($"{Mark} ar numurzīmi {Number} pārvietojas ar {Speed} km/h");
+        }
+        public void Stop()
+        {
+            Speed = 0;
+            Console.WriteLine($"{Mark} ar numurzīmi {Number} ir apstājusies");
+        }
+        public void Beep()
+        {
+            Console.WriteLine($"Beep beep");
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,26 +3,55 @@
 // Simbolu daudzbveidībai programmā
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.WriteLine("1. Uzdevums: Telefons");
-Telephone telephone1 = new Telephone();
+//Console.WriteLine("1. Uzdevums: Telefons");
+//Telephone telephone1 = new Telephone();
 
-Console.WriteLine($"Ievadījāt telefonu ar šādiem parametriem:");
-Console.WriteLine($"Marka: {telephone1.Mark}");
-Console.WriteLine($"Modelis: {telephone1.Model}");
-Console.WriteLine($"Izmēri: {telephone1.Hight} X {telephone1.Width} X {telephone1.Depth}");
-Console.WriteLine();
-telephone1.Call();
-telephone1.Sms();
+//Console.WriteLine($"Ievadījāt telefonu ar šādiem parametriem:");
+//Console.WriteLine($"Marka: {telephone1.Mark}");
+//Console.WriteLine($"Modelis: {telephone1.Model}");
+//Console.WriteLine($"Izmēri: {telephone1.Hight} X {telephone1.Width} X {telephone1.Depth}");
+//Console.WriteLine();
+//telephone1.Call();
+//telephone1.Sms();
 
 
-Console.WriteLine();
-Console.WriteLine();
-Telephone telephone2 = new Telephone();
+//Console.WriteLine();
+//Console.WriteLine();
+//Telephone telephone2 = new Telephone();
 
-Console.WriteLine($"Ievadījāt telefonu ar šādiem parametriem:");
-Console.WriteLine($"Marka: {telephone2.Mark}");
-Console.WriteLine($"Modelis: {telephone2.Model}");
-Console.WriteLine($"Izmēri: {telephone2.Hight} X {telephone2.Width} X {telephone2.Depth}");
-Console.WriteLine();
-telephone2.Call();
-telephone2.Sms();
+//Console.WriteLine($"Ievadījāt telefonu ar šādiem parametriem:");
+//Console.WriteLine($"Marka: {telephone2.Mark}");
+//Console.WriteLine($"Modelis: {telephone2.Model}");
+//Console.WriteLine($"Izmēri: {telephone2.Hight} X {telephone2.Width} X {telephone2.Depth}");
+//Console.WriteLine();
+//telephone2.Call();
+//telephone2.Sms();
+
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine("2. Uzdevums: Auto");
+//Console.WriteLine();
+//Console.WriteLine();
+//Car car1 = new Car();
+
+//Console.WriteLine($"Ievadījāt auto ar šādiem parametriem:");
+//Console.WriteLine($"Marka: {car1.Mark}");
+//Console.WriteLine($"Numurzīme: {car1.Number}");
+//Console.WriteLine($"Ātrums: {car1.Speed}");
+//car1.AddSpeed();
+//car1.ReduceSpeed();
+//car1.Stop();
+//car1.Beep();
+//car1.StartDrive();
+
+//Car car2 = new Car();
+
+//Console.WriteLine($"Ievadījāt auto ar šādiem parametriem:");
+//Console.WriteLine($"Marka: {car2.Mark}");
+//Console.WriteLine($"Numurzīme: {car2.Number}");
+//Console.WriteLine($"Ātrums: {car2.Speed}");
+//car2.AddSpeed();
+//car2.ReduceSpeed();
+//car2.Stop();
+//car2.Beep();
+//car2.StartDrive();
